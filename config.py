6556c27e39375d362a3017767680b6d7f20b7a15
LeeSY0110 <112144874+LeeSY0110@users.noreply.github.com>
2023-08-31 16:34:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-1.cizar3lczvxa.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "leesiakyen-employee"
 customregion = "us-east-1"
 
