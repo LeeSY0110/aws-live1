c71c7936e0c3d9785dd1d9aacecd3f99cfe3d25c
LeeSY0110 <112144874+LeeSY0110@users.noreply.github.com>
2023-08-31 16:37:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.cizar3lczvxa.us-east-1.rds.amazonaws.com"
+customhost = "database-1.cpc3sv6dkw4o.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
